dc818cb8d62ea1123eeb815bcd8fa24ff3602c78
Muhammet Emin Gürzoğlu <gurzoglum@gmail.com>
2025-05-12 20:48:23 +0300

    .

--- FORYA/Assets/Scripts/Cannon/CannonSpawner.cs
+++ FORYA/Assets/Scripts/Cannon/CannonSpawner.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using Unity.Netcode;
 using System.Collections;
-using System.Linq;  // Linq, spawn noktalarını karıştırmak için
+using System.Collections.Generic;
+using System.Linq;
 
 [RequireComponent(typeof(NetworkObject))]
 public class CannonSpawner : NetworkBehaviour
 {
     [Header("Spawn Settings")]
     [SerializeField] private Transform[] spawnPoints;
-    [SerializeField] private Transform centerPoint;
+    [SerializeField] private Transform[] centerPoints;
     [SerializeField] private GameObject cannonPrefab;
 
     [Header("Timing")]
@@ -27,7 +28,7 @@ public class CannonSpawner : NetworkBehaviour
         
         if (spawnPoints == null || spawnPoints.Length == 0)
             Debug.LogError("CannonSpawner: No spawn points assigned!", this);
-        if (centerPoint == null)
+        if (centerPoints == null  || centerPoints.Length == 0)
             Debug.LogError("CannonSpawner: centerPoint not assigned!", this);
         if (cannonPrefab == null)
             Debug.LogError("CannonSpawner: cannonPrefab not assigned!", this);
@@ -50,23 +51,26 @@ public class CannonSpawner : NetworkBehaviour
         int waveCount = 1 + Mathf.FloorToInt(Time.timeSinceLevelLoad / difficultyRampInterval);
         int spawnCount = Mathf.Clamp(waveCount, 1, maxSimultaneousSpawns);
 
-        
-        var points = spawnPoints.OrderBy(_ => Random.value)
-                                .Take(spawnCount);
-
-        foreach (var point in points)
+        //choose a spawn point and assign so no
+        List<Transform> available = new List<Transform>(spawnPoints);
+        for (int i = 0; i < spawnCount; i++)
         {
-            SpawnSingleCannon(point);
+            int spawnIndex = Random.Range(0, available.Count);
+            Transform spawnPoint = available[spawnIndex];
+            available.RemoveAt(spawnIndex);
+            
+            Transform center = centerPoints[Random.Range(0, centerPoints.Length)];
+            SpawnSingleCannon(spawnPoint, center);
         }
     }
 
-    private void SpawnSingleCannon(Transform spawnPoint)
+    private void SpawnSingleCannon(Transform spawnPoint,Transform centerPoint)
     {
-        //cannons look towards play area
+        //look towards center pos
         Vector3 dir = (centerPoint.position - spawnPoint.position).normalized;
         Quaternion rot = Quaternion.LookRotation(dir) * Quaternion.Euler(0f, -90f, 0f);
 
-        //spawning
+        //instantiation
         var cannon = Instantiate(cannonPrefab, spawnPoint.position, rot);
         cannon.GetComponent<NetworkObject>().Spawn(true);
     }
