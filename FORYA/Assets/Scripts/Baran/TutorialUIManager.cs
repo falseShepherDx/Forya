89f8124865e64605715979d0bf8606f088b76440
Muhammet Emin Gürzoğlu <gurzoglum@gmail.com>
2025-06-18 07:48:25 +0300

    Update TutorialUIManager.cs

--- FORYA/Assets/Scripts/Baran/TutorialUIManager.cs
+++ FORYA/Assets/Scripts/Baran/TutorialUIManager.cs
@@ -10,7 +10,7 @@ public class TutorialUIManager : NetworkBehaviour
     [SerializeField] GameObject canvas;
     [SerializeField] TextMeshProUGUI countdown;
 
-    private const int countdownSecond = 1;
+    private const int countdownSecond = 10;
 
     public override void OnNetworkSpawn()
     {
